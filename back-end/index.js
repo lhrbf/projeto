07870eef6d8ef0dfc39d04eddb51f9daf42df962
David Felipe <david.felipe2904@gmail.com>
2023-12-19 04:56:50 -0300

    Update 4

--- back-end/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import cors from "cors";
-
-const app=express();
-app.use(cors());
-app.get("/getData",(req,res)=> {
-    res.send("");
-});
-app.listen(5000,()=>console.log("app is running"));
